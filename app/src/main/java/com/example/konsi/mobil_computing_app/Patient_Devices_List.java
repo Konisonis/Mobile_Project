3e0b6649766358fc4223d59f366ce1a42adc9edc
Konstantin Rosenberg <konsi.rs@web.de>
2018-12-28 18:14:09 +0100

    started working on patient part

---
+++ app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Devices_List.java
@@ -0,0 +1,36 @@
+package com.example.konsi.mobil_computing_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Patient_Devices_List extends AppCompatActivity {
+
+    public static final String selected_device = "SELECTED_DEVICE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Test List Elements
+        final String[] items = new String[] {"Device1", "Device2", "Device3", "Device4"};
+        final Intent intent = new Intent(this, Patient_Main_Page.class);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient__devices__list);
+
+        ListView listView = (ListView) findViewById(R.id.ListView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                intent.putExtra(selected_device, items[position]);
+                startActivity(intent);
+            }
+        });
+    }
+}
