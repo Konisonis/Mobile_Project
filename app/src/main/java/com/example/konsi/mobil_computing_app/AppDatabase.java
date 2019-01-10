8fe881a52dd45da641eb30d033f5d43af67dd6e4
Konstantin Rosenberg <konsi.rs@web.de>
2019-01-10 17:24:59 +0100

    implemented database as singelton

--- app/src/main/java/com/example/konsi/mobil_computing_app/AppDatabase.java
+++ app/src/main/java/com/example/konsi/mobil_computing_app/AppDatabase.java
@@ -1,10 +1,31 @@
 package com.example.konsi.mobil_computing_app;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {ListPatient.class,ListDoctor.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract PatientDao patientDao();
     public abstract DoctorDao docterDao();
+
+    private static AppDatabase INSTANCE;
+
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "user-database")
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                            .allowMainThreadQueries()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
