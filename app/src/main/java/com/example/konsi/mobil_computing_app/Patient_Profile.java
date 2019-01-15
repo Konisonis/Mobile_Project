e838230ba85b54e80ccc6c18a661752613764e76
lehongtrang272 <lehongtrang1992@gmail.com>
2019-01-15 20:48:45 +0100

    password popup added

--- app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Profile.java
+++ app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Profile.java
@@ -5,14 +5,28 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class Patient_Profile extends AppCompatActivity {
     private static final String TAG = "MyActivity";
+    PopupWindow popUp;
+    LinearLayout layout;
+    TextView tv;
+    ViewGroup.LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +42,34 @@ public class Patient_Profile extends AppCompatActivity {
             userName.setText(settings.getString("Fullname", "Not found"));
             dob.setText(settings.getString("Dob", "N/A"));
         }
+
+        popUp = new PopupWindow(this);
+        layout = new LinearLayout(this);
+        tv = new TextView(this);
+        but = findViewById(R.id.changePassword);
+        but.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (click) {
+                    popUp.showAtLocation(layout, Gravity.TOP, 5, -50);
+                    popUp.update(0, -10, 600, 800);
+                    click = false;
+                } else {
+                    popUp.dismiss();
+                    click = true;
+                }
+            }
+
+        });
+        params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        tv.setText("Hi this is a sample text for popup window");
+        layout.addView(tv, params);
+        popUp.setContentView(layout);
+        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
