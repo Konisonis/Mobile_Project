77ace7ea59cad595b6b220a455efa945f1220d42
Konstantin Rosenberg <konsi.rs@web.de>
2019-01-20 16:11:45 +0100

    added master class for patient menu

--- app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Main_Page.java
+++ app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Main_Page.java
@@ -1,17 +1,12 @@
 package com.example.konsi.mobil_computing_app;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
-public class Patient_Main_Page extends AppCompatActivity {
+public class Patient_Main_Page extends Patient_MasterActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,41 +36,4 @@ public class Patient_Main_Page extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        Intent profileIntent = new Intent(this, Patient_Profile.class);
-        Intent devicesIntent = new Intent(this, Patient_Devices_List.class);
-        Intent infoIntent = new Intent(this, Patient_App_Info.class);
-        Intent logoutIntent = new Intent(this, LoginActivity.class);
-        Intent messageIntent = new Intent(this,Patient_Messages.class);
-
-
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.profile:
-                startActivity(profileIntent);
-                return true;
-            case R.id.message:
-                startActivity(messageIntent);
-                return true;
-            case R.id.devices:
-                startActivity(devicesIntent);
-                return true;
-            case R.id.info:
-                startActivity(infoIntent);
-                return true;
-            case R.id.logout:
-                //Remove all user data and the stack before starting new intent
-                logoutIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.clear();
-                editor.apply();
-                startActivity(logoutIntent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
--- app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Profile.java
+++ app/src/main/java/com/example/konsi/mobil_computing_app/Patient_Profile.java
@@ -1,31 +1,19 @@
 package com.example.konsi.mobil_computing_app;
 
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Patient_Profile extends AppCompatActivity {
+public class Patient_Profile extends Patient_MasterActivity {
     private static final String TAG = "MyActivity";
     Button but;
     boolean click = true;
@@ -79,43 +67,6 @@ public class Patient_Profile extends AppCompatActivity {
 
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        Intent profileIntent = new Intent(this, Patient_Profile.class);
-        Intent devicesIntent = new Intent(this, Patient_Devices_List.class);
-        Intent infoIntent = new Intent(this, Patient_App_Info.class);
-        Intent logoutIntent = new Intent(this, LoginActivity.class);
-        Intent messageIntent = new Intent(this,Patient_Messages.class);
-
-
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.profile:
-                startActivity(profileIntent);
-                return true;
-            case R.id.message:
-                startActivity(messageIntent);
-                return true;
-            case R.id.devices:
-                startActivity(devicesIntent);
-                return true;
-            case R.id.info:
-                startActivity(infoIntent);
-                return true;
-            case R.id.logout:
-                //Remove all user data and the stack before starting new intent
-                logoutIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.clear();
-                editor.apply();
-                startActivity(logoutIntent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
     public void onClick(View view){
         Log.d(TAG, "Password changed ");
     }
